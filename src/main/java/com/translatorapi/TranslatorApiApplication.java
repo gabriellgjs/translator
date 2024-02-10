3045ce3ad049ca0859910d219c9b4ce12c2f3f12
gabriellgjs <contato.gabrieljosesilva@gmail.com>
2024-02-10 15:38:52 -0300

    feat: init project

---
+++ src/main/java/com/translatorapi/TranslatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.translatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/translatorapi/TranslatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
